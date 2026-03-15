6998370e40b661cacea92a47b76efaf8a868e99c
Tali <thaliastafford@gmail.com>
2026-03-15 17:14:41 -0400

    Module 2 completed baseline

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET travel page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('travel', { title: 'Travlr Getaways' });
 });
 
 module.exports = router;
